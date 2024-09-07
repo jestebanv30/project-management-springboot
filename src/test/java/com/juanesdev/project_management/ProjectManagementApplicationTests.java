83e8793464e169e1a6ce4d1cb2fc587f5610f91d
Juan Esteban <valdezjuanes014@gmail.com>
2024-09-06 22:26:06 -0500

    first commit

---
+++ src/test/java/com/juanesdev/project_management/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanesdev.project_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
